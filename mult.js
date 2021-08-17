95102e496fd2365d32ddfc16d47cdfe3326d69c8
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2021-08-17 09:42:47 -0300

    Final

--- mult.js
+++ mult.js
@@ -1,9 +1,9 @@
 function mult(){
     let tabuada = []
     let multiplicacao = 0
-    for(let i = 1; i <=10; i++){
+    for(let i = 0; i <=10; i++){
         tabuada[i] = [i]
-        for(let j = 1; j<= 10; j++){
+        for(let j = 0; j<= 10; j++){
             multiplicacao = j * i
         tabuada[i][j] = multiplicacao
         }
